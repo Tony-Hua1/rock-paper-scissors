2b4c67ac612a3cdb25402882568ff7ce245ffe4b
Tony Hua <tony7068@gmail.com>
2022-03-09 19:25:45 -0800

    Create function that randomly returns 'Rock', 'Paper', or 'Scissors'

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello, World!");
+
+function computerPlay() {
+	let randomNumber = Math.floor(Math.random() * 3) + 1;
+	let computerSelection = "";
+
+	switch(randomNumber) {
+		case 1:
+			computerSelection = "Rock";
+			break;
+		case 2:
+			computerSelection = "Paper";
+			break;
+		case 3:
+			computerSelection = "Scissors";
+			break;
+		default:
+			computerPlay = "";
+	}
+
+	return computerSelection;
+}
