9d2446152d2d3cda849d1bfa2b7f5380c93807e2
Tony Hua <tony7068@gmail.com>
2022-03-28 20:22:19 -0700

    Remove logic that plays five rounds

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// let playerSelection = "Paper";
-
 game();
-
-// let computerSelection = computerPlay();
-
-// console.log(playRound(playerSelection, computerSelection));
-
-// console.log(playerSelection);
-// console.log(computerSelection);
 
 function computerPlay() {
 	let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -61,31 +52,9 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-	let playerScore = 0;
-	let computerScore = 0;
-
-	for (let i = 0; i < 5; ++i) {
-		let playerSelection = prompt("Pick Rock, Paper, or Scissors: ");
-		let computerSelection = computerPlay();
-		let str = playRound(playerSelection, computerSelection);
-
-		if (str.substring(0, 7) == "You win") {
-			playerScore += 1;
-		} else if (str.substring(0, 8) == "You lose") {
-			computerScore += 1;
-		}
-
-		console.log(playRound(playerSelection, computerSelection));
-	}
-
-	console.log(`\nYour Score: ${playerScore}`);
-	console.log(`Computer Score: ${computerScore}`);
-
-	if (playerScore > computerScore) {
-		console.log("You won the game!");
-	} else if (playerScore < computerScore) {
-		console.log("You lost the game!");
-	} else {
-		console.log("It was a draw!");
-	}
+	let playerSelection = prompt("Pick Rock, Paper, or Scissors: ");
+	let computerSelection = computerPlay();
+	let str = playRound(playerSelection, computerSelection);
+	
+	console.log(playRound(playerSelection, computerSelection));
 }
