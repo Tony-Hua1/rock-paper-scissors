a62050d8e27795200e21b4018157930a8257335b
Tony Hua <tony7068@gmail.com>
2022-03-28 23:37:45 -0700

    Allow player to play the game by clicking on buttons

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-game();
+let div = document.querySelector("div");
+let playerScore = 0;
+let computerScore = 0;
+
+let rock = document.getElementById("rock");
+rock.addEventListener("click", function() {
+	str = playRound("Rock", computerPlay());
+	div.innerHTML += str + "<br />";
+	checkScore(str);
+});
+
+let paper = document.getElementById("paper");
+paper.addEventListener("click", function() {
+	str = playRound("Paper", computerPlay());
+	div.innerHTML += str + "<br />";
+	checkScore(str);
+});
+
+let scissors = document.getElementById("scissors");
+scissors.addEventListener("click", function() {
+	str = playRound("scissors", computerPlay());
+	div.innerHTML += str + "<br />";
+	checkScore(str);
+});
+
+function checkScore(str) {
+	if (playerScore == 5 || computerScore == 5) {
+		div.innerHTML += "<br />" + `Your Score: ${playerScore}` + "<br />";
+		div.innerHTML += `Computer Score: ${computerScore}` + "<br />";
+		
+		if (playerScore > computerScore) {
+			div.innerHTML += "You won the game!";
+		} else if (playerScore < computerScore) {
+			div.innerHTML += "You lost the game!";
+		} else {
+			div.innerHTML += "It is a draw game!";
+		}
+		
+		div.innerHTML += "<br />" + "<br />";
+		playerScore = 0;
+		computerScore = 0;
+	} else {
+		if (str.substring(0, 7) == "You win") {
+			playerScore++;
+		} else if (str.substring(0, 8) == "You lose") {
+			computerScore++;
+		}
+	}
+}
 
 function computerPlay() {
 	let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -49,12 +97,4 @@ function playRound(playerSelection, computerSelection) {
 			return "It's a draw!";
 		}	
 	}
-}
-
-function game() {
-	let playerSelection = prompt("Pick Rock, Paper, or Scissors: ");
-	let computerSelection = computerPlay();
-	let str = playRound(playerSelection, computerSelection);
-	
-	console.log(playRound(playerSelection, computerSelection));
 }
